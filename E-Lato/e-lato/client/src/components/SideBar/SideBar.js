68e55c8ca5edfc681b22a83cc02ec4512c65b3c5
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-09-20 12:15:09 -0400

    added images
    added images

--- E-Lato/e-lato/client/src/components/SideBar/SideBar.js
+++ E-Lato/e-lato/client/src/components/SideBar/SideBar.js
@@ -1,50 +1,45 @@
 import React from "react";
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-import English from '../../assets/images/logos/English-01.svg';
-import Art from '../../assets/images/logos/Art-01.svg';
-import Math from '../../assets/images/logos/Math-01.svg';
-import Misc from '../../assets/images/logos/Misc-01.svg';
-import Music from '../../assets/images/logos/Music-01.svg';
-import Science from '../../assets/images/logos/Science-01.svg';
 import './_sidebar.scss';
+import Images from "../../assets/images";
 
 
 export const SideBarData = [
     {
         title: 'English',
         path: '/English',
-        icon:  `${English}`,
+        icon:  `${Images[0].English}`,
         cName: 'nav-text'
     },
     {
         title: 'Math',
         path: '/Math',
-        icon: `${Math}`,
+        icon: `${Images[0].Math}`,
         cName: 'nav-text'
     },
     {
         title: 'Music',
         path: '/Music',
-        icon: `${Music}`,
+        icon: `${Images[0].Music}`,
         cName: 'nav-text'
     },
     {
         title: 'Science',
         path: '/Science',
-        icon: `${Science}`,
+        icon: `${Images[0].Science}`,
         cName: 'nav-text'
     },
     {
         title: 'Art',
         path: '/Art',
-        icon: `${Art}`,
+        icon: `${Images[0].Art}`,
         cName: 'nav-text'
     },
     {
         title: 'Misc',
         path: '/Misc',
-        icon: `${Misc}`,
+        icon: `${Images[0].Misc}`,
         cName: 'nav-text'
     },
 ]
