799297c0a797a7106f0f50ce9aba075c7ae4f5ac
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-09-27 16:12:24 -0400

    created other page templates
    created other page templates

--- E-Lato/e-lato/client/src/App.js
+++ E-Lato/e-lato/client/src/App.js
@@ -2,7 +2,12 @@ import Header from './components/Header/Header';
 import HeroMain from './components/HeroMain/HeroMain';
 import LowerContent from './components/LowerContent/LowerContent';
 import Footer from './components/Footer/Footer';
-import ArtPage from './components/Pages/ArtPage';
+import ArtPage from './components/Pages/ArtPage/ArtPage';
+import MusicPage from './components/Pages/MusicPage/MusicPage';
+import EnglishPage from './components/Pages/EnglishPage/EnglishPage';
+import MathPage from './components/Pages/MathPage/MathPage';
+import MiscPage from './components/Pages/MiscPage/MiscPage';
+import SciencePage from './components/Pages/SciencePage/SciencePage';
 import './App.scss';
 import { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
@@ -13,43 +18,60 @@ import MessagingMain from "./components/MessagingMain/MessagingMain";
 
 class App extends Component {
 
-  state = {
-    jwtToken: false
-  }
-
-
-  checkToken = () => {
-    if (sessionStorage.getItem('jwtToken')){
-        this.setState( {jwtToken : !this.state.jwtToken})
+    state = {
+        jwtToken: false
     }
-}
 
-componentDidMount(){
-  this.checkToken()
-}
+
+    checkToken = () => {
+        if (sessionStorage.getItem('jwtToken')) {
+            this.setState({ jwtToken: !this.state.jwtToken })
+        }
+    }
+
+    componentDidMount() {
+        this.checkToken()
+    }
 
     render() {
         return (
             <div className="app">
                 <BrowserRouter>
-                <Header  token={this.state.jwtToken} />
+
+                    <Header token={this.state.jwtToken} />
+
                     <Switch>
-                    <Route path='/' exact>
-                    <HeroMain />
-                    <LowerContent/>
-                    </Route>
-                    <Route path='/art' exact>
-                    <ArtPage />
-                    </Route>
-                        
-                            
-                            <Route exact path="/profile" render={(props) => (<Profile checkToken={this.checkToken} {...props}  />)}/>
-                    
+
+                        <Route path='/' exact>
+                            <HeroMain />
+                            <LowerContent />
+                        </Route>
+
+                        <Route path='/art' exact>
+                            <ArtPage />
+                        </Route>
+                        <Route path='/music' exact>
+                            <MusicPage />
+                        </Route>
+                        <Route path='/english' exact>
+                            <EnglishPage />
+                        </Route>
+                        <Route path='/misc' exact>
+                            <MiscPage />
+                        </Route>
+                        <Route path='/math' exact>
+                            <MathPage />
+                        </Route>
+                        <Route path='/science' exact>
+                            <SciencePage />
+                        </Route>
+                        <Route exact path="/profile" render={(props) => (<Profile checkToken={this.checkToken} {...props} />)} />
+
                         <Route exact path='/messages'>
-                          <MessagingMain/>
-                          </Route>
-                          <Route exact path="/login" render={(props) => (<Login checkToken={this.checkToken} {...props}  />)}/>
-                        
+                            <MessagingMain />
+                        </Route>
+                        <Route exact path="/login" render={(props) => (<Login checkToken={this.checkToken} {...props} />)} />
+
                         <Route exact path="/signup">
                             <Signup />
                         </Route>
---
+++ E-Lato/e-lato/client/src/components/Pages/SciencePage/SciencePage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import ImageNavBar from '../../ImageNavBar/ImageNavBar';
+
+
+export default function SciencePage() {
+    return (
+        <main>
+            <div className='main__image'>
+            </div>
+            <ImageNavBar />
+
+            <div className='main__page-title'>
+                <h1>Art is the expression of the soul</h1>
+            </div>
+
+            <div className='main__page-news'>
+
+                <div className='main__trending'>
+
+                    <h2>Trending Classes</h2>
+
+                    <div className='main__trending-block'>
+
+                        <h3>Painting</h3>
+                        <img className='main__trending-image' src={''} alt='Painting Image' />
+
+                    </div>
+                    <div className='main__trending-block'>
+                        
+                        <h3>Caligraphy</h3>
+                        <img className='main__trending-image' src={''} alt='Caligraphy' />
+
+                    </div>
+                    
+                    <div className='main__trending-block'>
+                        
+                        <h3>Digital Art</h3>
+                        <img className='main__trending-image' src={''} alt='Digital Art' />
+
+                    </div>
+                </div>
+            </div>
+
+            <Link className='main__link' to='/techers'>
+            <button className='main__page-button'>Find a teacher</button>
+            </Link>
+
+        </main>
+    )
+}
